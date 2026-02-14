937a5a910b606ad25f2ac0d59fab2514dcd86be7
NicolasBittencourt05 <nicobelesck@gmail.com>
2026-02-14 12:39:20 -0300

    first commit

---
+++ src/test/java/nicobb/myfirstspringboot/MyFirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package nicobb.myfirstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
